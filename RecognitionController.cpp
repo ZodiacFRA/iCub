89e28fe9c9481eca87a0213894ad83662fc8814e
ZodiacFRA <jean-baptiste.dupuy@epitech.eu>
2019-09-27 19:30:37 +0100

    Added face detection with OpenCV (should work, could not test because remote prevents me from connecting webcam to world's screen)

--- RecognitionController.cpp
+++ RecognitionController.cpp
@@ -12,6 +12,9 @@ RecognitionController::RecognitionController()
 
 int RecognitionController::init()
 {
+	// PreDefined trained XML classifiers with facial features
+	// Load classifiers from the "opencv/data/haarcascades" directory
+	_cascade.load( "/usr/share/OpenCV/haarcascades/haarcascade_frontalface_alt.xml");
 	return SUCCESS;
 }
 
@@ -19,8 +22,49 @@ int RecognitionController::recognize(ImageOf<PixelRgb> *image,
 	std::vector<Vector> &faces, std::vector<Vector> &objects)
 {
 	getBlueTargetPosition(image, objects);
+
+	// Transform yarp image into openCV Mat
+	cv::Mat frame((IplImage*)image->getIplImage());
+    // cv::cvtColor(frame, frame, CV_RGB2BGR);
+
+	getFacesPositions(frame, _cascade, faces);
 	return SUCCESS;
 }
+
+
+int RecognitionController::getFacesPositions(cv::Mat &img,
+	cv::CascadeClassifier &cascade, std::vector<Vector> &faces)
+{
+	Vector tmp;
+	tmp.resize(3);
+    std::vector<cv::Rect> CVfaces;
+    cv::Mat gray, smallImg;
+
+    cvtColor(img, gray, cv::COLOR_BGR2GRAY); // Convert to Gray Scale
+
+    // Resize the Grayscale Image
+    resize(gray, smallImg, cv::Size(), 1, 1, cv::INTER_LINEAR);
+    equalizeHist(smallImg, smallImg);
+    // Detect faces of different sizes using cascade classifier
+    cascade.detectMultiScale(smallImg, CVfaces, 1.1,2, 0|cv::CASCADE_SCALE_IMAGE, cv::Size(30, 30));
+
+	// printf("size=%d\n\0", CVfaces.size());
+    // Draw circles around the faces
+    for (auto r : CVfaces) {
+        cv::Point center;
+        int radius;
+        center.x = cvRound((r.x + r.width*0.5));
+        center.y = cvRound((r.y + r.height*0.5));
+        radius = cvRound((r.width + r.height)*0.25);
+		tmp[0] = center.x;
+		tmp[1] = center.y;
+		tmp[2] = radius;
+		faces.push_back(tmp);
+        printf("FACE x= %d, y= %d, radius= %d\n", center.x, center.y, radius);
+    }
+	return SUCCESS;
+}
+
 
 // Find position of the target in image (will fail with multiple targets)
 int RecognitionController::getBlueTargetPosition(ImageOf<PixelRgb> *image, std::vector<Vector> &objects)
--- RecognitionController.hpp
+++ RecognitionController.hpp
@@ -3,6 +3,10 @@
 
 #include "common.hpp"
 
+#include "/usr/include/opencv2/objdetect/objdetect.hpp"
+#include "/usr/include/opencv2/highgui/highgui.hpp"
+#include "/usr/include/opencv2/imgproc/imgproc.hpp"
+
 using namespace yarp::os;
 using namespace yarp::sig;
 using namespace yarp::dev;
@@ -11,6 +15,7 @@ using namespace yarp::dev;
 class RecognitionController {
 private:
 	Vector _oldTargetPos;
+	cv::CascadeClassifier _cascade;
 
 public:
 	RecognitionController();
@@ -18,6 +23,7 @@ public:
 	int init();
 	int recognize(ImageOf<PixelRgb> *, std::vector<Vector> &, std::vector<Vector> &);
 	int getBlueTargetPosition(ImageOf<PixelRgb> *, std::vector<Vector> &);
+	int getFacesPositions(cv::Mat &, cv::CascadeClassifier &, std::vector<Vector> &);
 };
 
 #endif
