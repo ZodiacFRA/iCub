231224434530049dc6619cf00693854ed491dcce
Cesar Gutierrez Carrero <cesarguti47@gmail.com>
2019-10-03 16:39:32 +0100

    Fixing filter method

--- VisionController.hpp
+++ VisionController.hpp
@@ -4,6 +4,7 @@
 #include "common.hpp"
 // #include "opencv2/imgproc/imgproc.hpp"
 // #include "opencv2/highgui/highgui.hpp"
+#include "/usr/include/opencv.hpp"
 #include "/usr/include/opencv2/highgui/highgui.hpp"
 #include "/usr/include/opencv2/imgproc/imgproc.hpp"
 
