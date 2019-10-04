0515957e46f71c2c2b1d27891e8b2c386c2bba8c
Cesar Gutierrez Carrero <cesarguti47@gmail.com>
2019-10-04 18:26:00 +0100

    Test: 8.10

--- RecognitionController.cpp
+++ RecognitionController.cpp
@@ -26,7 +26,7 @@ int RecognitionController::recognize(ImageOf<PixelRgb> *image,
 	// Transform yarp image into openCV Mat
 	cv::Mat frame((IplImage*)image->getIplImage());
 	cv::Mat *img = new cv::Mat((IplImage*)image->getIplImage());
-	VisionController::filterImage(*img);
+	VisionController::filterImage(img);
   cv::cvtColor(frame, frame, CV_RGB2BGR);
 
 	getFacesPositions(frame, _cascade, faces);
