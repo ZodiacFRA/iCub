ace9e72d25ffe4a1fbbcfb86ebf5e7f2a460ca4e
Cesar Gutierrez Carrero <cesarguti47@gmail.com>
2019-10-05 12:42:56 +0100

    Debug 1.2

--- VisionController.cpp
+++ VisionController.cpp
@@ -50,7 +50,7 @@ int VisionController::getRobotView(ImageOf<PixelRgb> **image)
 		_receiveFlag = true;
 	}
 
-	printf(image->getRawImage());
+	printf((char *)image->getRawImage());
 	// filterImage(new ImageOf<PixelRgb>(**image));
 
 	return SUCCESS;
