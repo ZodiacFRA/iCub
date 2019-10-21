a2d0b0a1ed80c635b10212bd79ba392da2afc379
ZodiacFRA <37930951+ZodiacFRA@users.noreply.github.com>
2019-10-21 14:19:58 +0000

    Update env_setup.sh

--- env_setup.sh
+++ env_setup.sh
@@ -2,11 +2,11 @@
 
 xterm -e "yarpserver --write" &
 # xterm -e "cd Downloads/IR_assignment/iCub/ ; python3 box_mover.py"
-xterm -e "cd Downloads/IR_assignment/iCub/ ; iCub_SIM" &
+xterm -e "iCub_SIM" &
 sleep 5
 xterm -e "yarpdev --device opencv_grabber --name /test/video" &
 sleep 5
 xterm -e "yarp connect /test/video /icubSim/texture/screen"
 xterm -e "yarpview --name /view/left" &
 sleep 3
-xterm -e "cd Downloads/IR_assignment/iCub/ ; ./icubSim"
+xterm -e "./icubSim"
